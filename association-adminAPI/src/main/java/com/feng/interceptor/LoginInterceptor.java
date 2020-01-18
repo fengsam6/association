09b2b4a07bd6e3541074dcf7a93f03cfbd739198
feng <1073612350@qq.com>
2020-01-18 23:18:49 +0800

    完善后端登录拦截

--- association-adminAPI/src/main/java/com/feng/interceptor/LoginInterceptor.java
+++ association-adminAPI/src/main/java/com/feng/interceptor/LoginInterceptor.java
@@ -1,9 +1,13 @@
 package com.feng.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import com.feng.entity.ResponseResult;
 import com.feng.enums.ErrorEnum;
 import com.feng.exception.AuthenticationFailException;
 import com.feng.service.UserSessionService;
+import com.feng.util.ResponseResultUtil;
 import com.feng.util.UserTokenUtils;
+import com.mysql.cj.jdbc.util.ResultSetUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,8 +17,12 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 /**
  * Created by rf on 2019/4/23.
@@ -29,29 +37,57 @@ public class LoginInterceptor implements HandlerInterceptor {
     private boolean isOpenTestDev = false;
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //如果开启测试环境，所有请求放行
-        if (isOpenTestDev)
-            return true;
-        String token = UserTokenUtils.getUserToken(request);
 
-        if (!StringUtils.isEmpty(token)) {
-            //判断用户token是否有效
-            boolean exist = userSessionService.isUserTokenExist(token);
-            if (exist) {
-                userSessionService.updateUserSession(token);
+        String token = UserTokenUtils.getUserToken(request);
+        String url = request.getRequestURI();
+        if (StringUtils.isEmpty(token)) {
+            //如果开启测试环境，所有请求放行
+            if (isOpenTestDev) {
+                log.info("请求url：{}，token不存在。由于开启测试环境，不校验token是否为空、有效", url);
                 return true;
             }
+            renderJsonError(response, "没有授权,请先登录");
+            return false;
         }
 
-        throw new AuthenticationFailException(ErrorEnum.USER_NOT_LOGIN);
+
+        //判断用户token是否有效
+        boolean exist = userSessionService.isUserTokenExist(token);
+        if (exist) {
+            log.info("更新token在redis中会话时间");
+            userSessionService.updateUserSession(token);
+            return true;
+        } else {
+            //如果开启测试环境，所有请求放行
+            if (isOpenTestDev) {
+                log.info("请求url：{}，token无效，由于开启测试环境，不校验token有效性", url);
+                return true;
+            }
+
+        }
+        renderJsonError(response, "没有授权,请先登录");
+        return false;
 
     }
 
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
 
-    }
+    private void renderJsonError(HttpServletResponse response, String msg) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        ResponseResult responseResult = ResponseResultUtil.renderError(ErrorEnum.UNAUTHORIZED.setMsg(msg));
+        String errorJson = JSON.toJSONString(responseResult);
+        PrintWriter printWriter = null;
+        try {
+            printWriter = response.getWriter();
+            printWriter.print(errorJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (printWriter != null)
+                printWriter.close();
+        }
 
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
     }
 
 }
--- association-restAPI/src/main/java/com/feng/config/WebConfigurer.java
+++ association-restAPI/src/main/java/com/feng/config/WebConfigurer.java
@@ -1,10 +1,7 @@
 package com.feng.config;
 
-import com.feng.interceptor.LoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -13,16 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  */
 @Configuration
 public class WebConfigurer extends WebMvcConfigurationSupport {
-    @Autowired
-    private LoginInterceptor loginInterceptor;
-    @Value("${system.openDevEnv}")
-    private boolean isOpenTestDev = false;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/users/login","/users/getKaptcha","/logout","/system/frontUrl");
-        super.addInterceptors(registry);
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
